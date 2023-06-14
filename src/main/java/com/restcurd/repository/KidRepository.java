0964d91ad61c03680314509a8f898d5a51385095
Gopi Thangavel <gopi.t@veetechnologies.com>
2023-06-14 20:08:13 +0530

    spring rest crud added

--- src/main/java/com/restcurd/repository/KidRepository.java
+++ src/main/java/com/restcurd/repository/KidRepository.java
@@ -2,8 +2,11 @@ package com.restcurd.repository;
 
 import com.restcurd.entity.Kid;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
+
+//@RepositoryRestResource(path = "members")
 public interface KidRepository extends JpaRepository<Kid,Long> {
 }
